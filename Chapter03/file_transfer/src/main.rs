b240d6e5c339ce8fc6e89613fd7479232d579784
hugh <alicorn0618@outlook.com>
2024-05-14 12:23:52 +0800

    chore: Update database connection details
    This commit updates the database connection details in the `config.toml` file. The Redis host is changed to `redis://:2887@202.120.40.114:36379`, and the PostgreSQL username, password, host, port, and database are updated to `chenhao`, `2887`, `202.120.40.114`, `35432`, and `chenhao` respectively.

--- Chapter03/file_transfer/src/main.rs
+++ Chapter03/file_transfer/src/main.rs
@@ -5,82 +5,18 @@
 // curl -X POST http://localhost:8080/data -d "File contents."
 // curl -X GET http://localhost:8080/a/b
 
-use std::io::Write;
-
-use actix_web::{
-    web::{self, Path},
-    App, Error, HttpResponse, HttpServer,
-};
+use actix_web::Error;
+use actix_web::{web, web::Path, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use futures::{
     future::{ok, Future},
-    StreamExt, TryStreamExt,
+    Stream,
 };
+use rand::prelude::*;
+use std::fs::{File, OpenOptions};
+use std::io::Write;
 
 fn flush_stdout() {
     std::io::stdout().flush().unwrap();
 }
 
-fn upload_new_file(
-    payload: web::Payload,
-    info: Path<(String,)>,
-) -> impl Future<Item = HttpResponse, Error = Error> {
-    let filename_prefix = info.0.clone();
-    print!("Uploading file \"{}*.txt\" ... ", filename_prefix);
-    flush_stdout();
-
-    payload
-        .map_err(Error::from)
-        .fold(web::BytesMut::new(), move |mut body, chunk| {
-            body.extend_from_slice(&chunk);
-            Ok::<_, Error>(body)
-        })
-        .and_then(move |contents| {
-            let mut rng = rand::thread_rng();
-            let mut attempts = 0;
-            let mut file;
-            let mut filename;
-            const MAX_ATTEMPTS: u32 = 100;
-
-            loop {
-                attempts += 1;
-                if attempts > MAX_ATTEMPTS {
-                    println!(
-                        "Failed to create new file with prefix \"{}\", \
-                         after {} attempts.",
-                        filename_prefix, MAX_ATTEMPTS
-                    );
-                    return ok(HttpResponse::NotFound().into());
-                }
-
-                // Generate a 3-digit pseudo-random number.
-                // and use it to create a file name.
-                filename = format!("{}{:03}.txt", filename_prefix, rng.gen_range(0, 1000));
-
-                // Create a not-yet-existing file.
-                file = OpenOptions::new()
-                    .write(true)
-                    .create_new(true)
-                    .open(&filename);
-
-                // If it was created, exit the loop.
-                if file.is_ok() {
-                    break;
-                }
-            }
-
-            // Write the contents into it synchronously.
-            if file.unwrap().write_all(&contents).is_err() {
-                println!("Failed to write file \"{}\"", filename);
-                return ok(HttpResponse::NotFound().into());
-            }
-
-            println!("Uploaded file \"{}\"", filename);
-            ok(HttpResponse::Ok().content_type("text/plain").body(filename))
-        })
-}
-fn main() -> std::io::Result<()> {
-    let server_address = "127.0.0.1:8080";
-    println!("Listening at address {}", server_address);
-    // HttpServer::new(|| {}).bind(server_address).run()
-    Ok(())
-}
+fn main() {}
